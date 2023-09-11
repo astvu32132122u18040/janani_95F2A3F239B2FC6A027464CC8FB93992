2ef8a323141f353191e6a8e571522137e99a6eb3
astvu32132122u18040 <144653897+astvu32132122u18040@users.noreply.github.com>
2023-09-11 11:28:39 +0530

    Add files via upload

---
+++ challenge 1.py
@@ -0,0 +1,15 @@
+def fact(n):
+  if n==0:
+    return 1
+  else:
+    return n*fact(n-1)
+print(fact(0))
+print(fact(2))
+print(fact(3))
+print(fact(4))
+print(fact(5))
+print(fact(6))
+print(fact(7))
+print(fact(8))
+print(fact(9))
+print(fact(10))
